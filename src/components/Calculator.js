63192b066181ed5ace7368777e8055a432252e1e
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-26 20:05:03 -0600

    Add first row of buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,18 @@
+import CalculatorButton from './CalculatorButton';
+
 function Calculator() {
   return (
     <>
-      <h1>Calculator component!</h1>
+      <h1>Calculator</h1>
+      <div>
+        <input type="text" readOnly />
+      </div>
+      <div>
+        <CalculatorButton value="AC" />
+        <CalculatorButton value="+/-" />
+        <CalculatorButton value="%" />
+        <CalculatorButton value="÷" />
+      </div>
     </>
   );
 }
