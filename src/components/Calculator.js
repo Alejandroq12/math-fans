768205dab2a0ba0d3b61a4818e15881d07245751
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-26 20:19:32 -0600

    Add classes to divs to make styling easier

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,34 +3,34 @@ import CalculatorButton from './CalculatorButton';
 function Calculator() {
   return (
     <div className="calculator">
-      <div>
+      <div className="calculator-display">
         <input type="text" readOnly />
       </div>
-      <div>
+      <div className="calculator-row">
         <CalculatorButton value="AC" />
         <CalculatorButton value="+/-" />
         <CalculatorButton value="%" />
         <CalculatorButton value="÷" />
       </div>
-      <div>
+      <div className="calculator-row">
         <CalculatorButton value="7" />
         <CalculatorButton value="8" />
         <CalculatorButton value="9" />
         <CalculatorButton value="x" />
       </div>
-      <div>
+      <div className="calculator-row">
         <CalculatorButton value="4" />
         <CalculatorButton value="5" />
         <CalculatorButton value="6" />
         <CalculatorButton value="-" />
       </div>
-      <div>
+      <div className="calculator-row">
         <CalculatorButton value="1" />
         <CalculatorButton value="2" />
         <CalculatorButton value="3" />
         <CalculatorButton value="+" />
       </div>
-      <div>
+      <div className="calculator-row">
         <CalculatorButton value="0" />
         <CalculatorButton value="." />
         <CalculatorButton value="=" />
