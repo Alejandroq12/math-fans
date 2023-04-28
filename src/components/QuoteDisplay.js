77154607b3d666ad6c3ff172a398fe053732f43b
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-28 15:51:49 -0600

    Add component to display quote

---
+++ src/components/QuoteDisplay.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
+function QuoteDisplay() {
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+          headers: { 'X-Api-Key': '/8HY4KlsVCA5B69kB4GDfw==G6WPzahEUAgdyVM1' },
+        });
+        setQuote(response.data[0]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+  return (
+    <div className="quote-container">
+      {loading && <p className="quote-loading">Loading...</p>}
+      {error && (
+        <p className="quote-error">
+          Error:
+          {error.message}
+        </p>
+      )}
+      {quote && (
+      <div>
+        <p className="quote-text">
+          {`"${quote.quote}"`}
+        </p>
+        <p className="quote-author">
+          -
+          {quote.author}
+        </p>
+      </div>
+      )}
+    </div>
+  );
+}
+
+export default QuoteDisplay;
