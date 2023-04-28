cf522fb4a290a1073d618d2b5f7163a1f8d79f6d
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-28 15:54:56 -0600

    Include the new component into app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Calculator from './components/Calculator';
+import QuoteDisplay from './components/QuoteDisplay';
 
 function App() {
   return (
     <div>
       <Calculator />
+      <QuoteDisplay />
     </div>
   );
 }
