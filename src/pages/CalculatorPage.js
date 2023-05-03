e1a23039d281a88b2557b9863d9135cccc990147
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 20:45:18 -0600

    Modify title

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -4,7 +4,7 @@ import Calculator from '../components/Calculator';
 function CalculatorPage() {
   return (
     <div className="calculator-page">
-      <h2>Calculator</h2>
+      <h2>Let&apos;s do some math.</h2>
       <Calculator />
     </div>
   );
