569461153b706ee69c73c2fa3cf2d3341d180618
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 18:56:55 -0600

    Add calculator page

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+function CalculatorPage() {
+  return (
+    <div className="calculator-page">
+      <h2>Calculator</h2>
+      <Calculator />
+    </div>
+  );
+}
+
+export default CalculatorPage;
