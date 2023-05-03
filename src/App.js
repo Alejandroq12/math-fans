ddd6e97d00c608c5ea27bbb0d23da55f4430d3ca
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 19:24:37 -0600

    Add routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Calculator from './components/Calculator';
-import QuoteDisplay from './components/QuoteDisplay';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import CalculatorPage from './pages/CalculatorPage';
+import QuotePage from './pages/QuotePage';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <QuoteDisplay />
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/quote" element={<QuotePage />} />
+      </Routes>
     </div>
   );
 }
