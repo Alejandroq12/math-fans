15789c0dc8492592f4a939c08abacc24da32ef4b
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 19:04:02 -0600

    Add quotes page

---
+++ src/pages/QuotePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import QuoteDisplay from '../components/QuoteDisplay';
+
+function QuotePage() {
+  return (
+    <div className="quote-page">
+      <h2>Quote</h2>
+      <QuoteDisplay />
+    </div>
+  );
+}
+
+export default QuotePage;
