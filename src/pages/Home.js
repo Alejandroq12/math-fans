4ff3ca93429dcf8631cc2d576a75b153b6b4ece4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 20:43:36 -0600

    Add className to each li element

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,40 +5,40 @@ function Home() {
     <div className="home">
       <h2>Welcome to Math Fans!</h2>
       <p>
-        The Math Fans website is a single-page application built using
-        React, showcasing my proficiency in creating functional and visually
-        appealing components. I have successfully implemented React Router for
-        navigation between different pages and utilized the Axios
-        library for fetching quotes from an external API.
+        The Math Fans website is a single-page application built using React,
+        showcasing my proficiency in creating functional and visually appealing
+        components. I have successfully implemented React Router for navigation
+        between different pages and utilized the Axios library for fetching
+        quotes from an external API.
       </p>
       <ul>
-        <li>
+        <li className="home-list">
           Implementing React Router for managing multiple pages within a
           single-page application.
         </li>
-        <li>
+        <li className="home-list">
           Creating reusable components, such as a navigation bar and calculator
           buttons.
         </li>
-        <li>
+        <li className="home-list">
           Handling component states and user interactions using hooks (useState
           and useEffect).
         </li>
-        <li>
+        <li className="home-list">
           Integrating external APIs using the Axios library for fetching quotes.
         </li>
-        <li>
+        <li className="home-list">
           Applying responsive CSS styles to ensure an aesthetically pleasing
           user interface.
         </li>
-        <p>
-          By completing this project, I have demonstrated a strong understanding
-          of React fundamentals, including state management, component
-          lifecycle, and routing. Furthermore, I have showcased my ability to
-          integrate third-party libraries and APIs, making me a well-rounded
-          developer suitable for modern web development roles.
-        </p>
       </ul>
+      <p>
+        By completing this project, I have demonstrated a strong understanding
+        of React fundamentals, including state management, component lifecycle,
+        and routing. Furthermore, I have showcased my ability to integrate
+        third-party libraries and APIs, making me a well-rounded developer
+        suitable for modern web development roles.
+      </p>
     </div>
   );
 }
